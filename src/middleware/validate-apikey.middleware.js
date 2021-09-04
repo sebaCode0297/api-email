ebdccc5b845b9d05d78dc013aa7c785c3002b71e
ccamilo <ccamilo@bancoestado.cl>
2021-09-04 15:18:55 -0400

    fix

--- src/middleware/validate-apikey.middleware.js
+++ src/middleware/validate-apikey.middleware.js
@@ -2,6 +2,7 @@ const environment = require("../config/env");
 
 const validateApiKey = (req, res, next) => {
   const { authorization } = req.headers;
+  console.log(`[validateApiKey]: authorization: ${authorization} APIKEY: ${environment.APIKEY}`);
   if (authorization === environment.APIKEY) next();
   else res.status(401).json({ message: "No estas autorizado." });
 };

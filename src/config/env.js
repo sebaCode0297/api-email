551116b52c2dee1d13cf4de5321051d85cd2af42
ccamilo <ccamilo@bancoestado.cl>
2021-09-04 01:41:17 -0400

    Se agrega variable PORT

---
+++ src/config/env.js
@@ -0,0 +1,5 @@
+const environment = {
+    PORT: process.env.PORT
+}
+
+module.exports = environment;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const cors = require("cors");
 const router = require("./routes/index");
+const environment = require('./config/env');
 
 const app = express();
 
@@ -10,7 +11,7 @@ app.use(express.json());
 
 app.use('/api/v1', router);
 
-const port = 3000;
+const port = environment.PORT || 3000;
 app.listen(port, () => {
   console.log("Server listening on port: " + port);
 });

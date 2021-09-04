14ab81c579a96f0cef3f9c73d251f1d494a0ad61
ccamilo <ccamilo@bancoestado.cl>
2021-09-04 15:55:02 -0400

    fix cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 "use strict";
 
 const express = require("express");
+const cors = require('cors');
+
 const router = require("./routes/index");
 const environment = require('./config/env');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1', router);
